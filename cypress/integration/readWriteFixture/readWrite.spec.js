60c96934f2f6082ad560a1fcccfcf499fa2139a3
Braian <braian_oroda@hotmail.com>
2022-04-09 20:51:01 -0300

    Sumando Lestura y escritura de archivos

---
+++ cypress/integration/readWriteFixture/readWrite.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+const filePath = 'cypress/fixtures/readWrite/readWrite.json';
+
+describe('Write/Read Fixture Demo', () => {
+    it('Agregar la respuesta de una API a un JSON', () => {
+        cy.request('GET', 'http://localhost:3000/todos')
+        .then(response => {
+            cy.log(response.body);
+            cy.writeFile(filePath, response.body)
+        });
+    });
+
+    // it('Escribir en un archivo la respuesta de una API a un JSON', () => {
+    //     cy.request('http://localhost:3000/todos')
+    //     .its('body')
+    //     .each( objeto => {
+    //         cy.readFile(filePath).then(array => {
+    //             array.push(objeto)
+
+    //             cy.writeFile(filePath, array)
+    //         })
+    //     });
+    // });
+});
